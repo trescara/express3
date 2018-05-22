ad84d0a266bb98bbe833a3d057124d7fea83d708
Shaun Carter <thereal.shauncarter@gmail.com>
2018-05-22 15:50:23 -0600

    adding express files.

---
+++ App.js
@@ -0,0 +1,48 @@
+const express = require('express');
+var morgan = require('morgan');
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+const students = require("./students.json")
+app.use(morgan('tiny'));
+
+// fetch("./students.json")
+//  .then(response => response.json())
+//  .then(json => console.log(json));
+
+
+
+function findById(data, id) {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id == id) {
+      return data[i];
+    }
+  }
+  return null;
+}
+
+
+app.use(cors());
+
+
+app.listen(port, () => {
+  app.get("/", function (request, response) {
+    response.json({ data: students });
+  });
+
+  app.get("/:id", function (request, response) {
+    var record = findById(students, request.params.id);
+    if (!record) {
+      response.status = 404;
+      response.json({
+        error: {
+          message: "No record found!"
+        }
+      });
+    }
+
+    response.json({ data: record });
+
+
+  })
+})
